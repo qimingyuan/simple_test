0501c9180ce21f4125061f89c0e0f143f6f35878
Qiming Yuan <yuanqiming@gmail.com>
2021-09-17 15:19:52 -0700

    Update test_add.py

--- test_add.py
+++ test_add.py
@@ -1,7 +1,14 @@
-from add import add_qiming
+from add import add_qiming, Add
 
 def test_add():
     assert add_qiming(1, 1) == 2
     assert add_qiming(1, 2) == 3
     assert add_qiming(3, 4) == 7
     assert add_qiming(6, 5) == 11
+
+def test_add_class():
+    add = Add()
+    assert add.apply(1, 1) == 2
+    assert add.apply(3, 4) == 7
+    assert add.apply(5, 6) == 11
+
